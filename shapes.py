3ab5b86d55de4d9ff215f57b92a5c4a2a4f13dcb
Federico <diningphil@users.noreply.github.com>
2016-09-02 23:05:48 +0200

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -3,31 +3,70 @@ import random
 class Piece:
 
     def __init__(self):
-        structure = None
+        self.idxRot = 0
+        self.structure = None
+        self.oldstructure = None
         self.row = 0
         self.oldrow = 0
         self.col = 0
         self.oldcol = 0
 
     def rotate(self):
-        raise NotImplementedError("Please implement this method")
+        self.idxRot = (self.idxRot + 1) % 4
+        if(self.idxRot == 0):
+            self.structure = self.standardRotation()
+        elif(self.idxRot == 1):
+            self.structure = self.rotateRight()
+        elif(self.idxRot == 2):
+            self.structure = self.flip()
+        elif(self.idxRot == 2):
+            self.structure = self.rotateLeft()
+            
+        
+    def rotateLeft(self):
+        raise Exception("Implement it")
+    def rotateRight(self):
+        raise Exception("Implement it")
+    def standardRotation(self):
+        raise Exception("Implement it")
+    def flip(self):
+        raise Exception("Implement it")
+    
 
 class Square(Piece):
-    structure = [(0,0), (0,1), (1,0), (1,1)]	
+    
+    def __init__(self):
+        Piece.__init__(self)
+        self.structure = [(0,0), (0,1), (1,0), (1,1)]	
 
-    def rotate(self):
-        pass
+    def rotateLeft(self):
+        return [(0,0), (0,1), (1,0), (1,1)]	
+    def rotateRight(self):
+        return [(0,0), (0,1), (1,0), (1,1)]	
+    def standardRotation(self):
+        return [(0,0), (0,1), (1,0), (1,1)]	
+    def flip(self):
+        return [(0,0), (0,1), (1,0), (1,1)]	
 
 class JPiece(Piece):
-    structure = [(0,0), (0,1), (1,1), (1,2)]
+    
+    def __init__(self):
+        Piece.__init__(self)
+        self.structure = [(0,0), (0,1), (1,1), (1,2)]
 
-    def rotate(self):
-        pass
-
-def pickRandomPiece():
-    randomIndex = random.randint(0,1)
-    if(randomIndex == 0):
-        return Square()
-    elif(randomIndex == 1):
-        return JPiece()
-    return Piece() 
+    def rotateLeft(self):
+        return [(0,1), (1,1), (1,0), (2,0)]	
+    def rotateRight(self):
+        return [(0,1), (1,1), (1,0), (2,0)]	
+    def standardRotation(self):
+        return [(0,0), (0,1), (1,1), (1,2)]
+    def flip(self):
+        return [(1,0), (1,1), (0,1), (0,2)]	
+     
+    def pickRandomPiece():
+        randomIndex = random.randint(0,1)
+        if(randomIndex == 0):
+            return Square()
+        elif(randomIndex == 1):
+            return JPiece()
+        return Piece() 
