ac790e0a79978f93eb48b6ebcbb102e08f5a0de3
Federico <f.errica93@gmail.com>
2016-09-03 16:21:58 +0200

    Added Stick shape

--- shapes.py
+++ shapes.py
@@ -63,13 +63,33 @@ class JPiece(Piece):
         return [(0,1), (1,1), (1,0), (2,0)]	
     def flip(self):
         return [(1,0), (1,1), (0,1), (0,2)]	
+
+class Stick(Piece):
+    
+    def __init__(self):
+        Piece.__init__(self)
+        self.structure = [(0,0), (0,1), (0,2)]
+        self.oldstructure = [(0,0), (0,1), (0,2)]
+
+    def standardPosition(self):
+        return [(0,0), (0,1), (0,2)]
+    def rotateLeft(self):
+        return [(0,0), (1,0), (2,0)]	
+    def rotateRight(self):
+        return [(0,0), (1,0), (2,0)]	
+    def flip(self):
+        return [(0,0), (0,1), (0,2)]	
      
 def pickRandomPiece():
-        randomIndex = random.randint(0,1)
+        randomIndex = random.randint(0,2)
         if(randomIndex == 0):
 	    print "Picked Square"
             return Square()
         elif(randomIndex == 1):
 	    print "Picked JPiece"
             return JPiece()
+        elif(randomIndex == 1):
+	    print "Picked Stick"
+            return Stick()
         return Square() 
+
